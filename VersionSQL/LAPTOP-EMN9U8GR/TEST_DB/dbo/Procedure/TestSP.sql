19d8b6ed56ea2ca560e017e53981fc3a1eaa2c7e
keathal <jemovey@gmail.com>
2023-02-02 15:31:32 +0700

    Comment from ssms stored procedure

--- VersionSQL/LAPTOP-EMN9U8GR/TEST_DB/dbo/Procedure/TestSP.sql
+++ VersionSQL/LAPTOP-EMN9U8GR/TEST_DB/dbo/Procedure/TestSP.sql
@@ -1,11 +1,12 @@
 ﻿/****** Object:  Procedure [dbo].[TestSP]    Committed by VersionSQL https://www.versionsql.com ******/
 
-CREATE PROCEDURE TestSP 
+CREATE PROCEDURE [dbo].[TestSP] 
 
 AS
 BEGIN
 	SET NOCOUNT ON;
 	
 -- comment from git editor
+-- comment from ssms
     select * from dbo.Notes order by [Value]
 END
